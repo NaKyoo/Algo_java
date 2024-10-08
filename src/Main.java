265fefea83d403b36b843e3561d19ed06d2810e8
Axel Letourneur <axel.letourneur-chiffoleau@edu.esiee-it.fr>
2024-10-08 11:01:07 +0200

    TP #01 with random

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int[] tabscores = new int[7];
         int moyenne = 0;
-        int Moyenne = 0;
+        float Moyenne = 0;
 
         tabscores[0] = 1;
         tabscores[1] = 2;
@@ -15,14 +15,14 @@ public class Main {
         tabscores[5] = 6;
         tabscores[6] = 7;
 
-        /*
+
         Random randomNumber = new Random();
 
         for(int i = 0; i < tabscores.length; i++) {
-            int scores  = randomNumber.nextInt(100);
-            scores[i] = scores;
+            int scores  = randomNumber.nextInt(101);
+            tabscores[i] = scores;
         }
-        */
+
 
         for (int i = 0; i < tabscores.length; i++) {
             int s = tabscores[i];
@@ -34,7 +34,8 @@ public class Main {
            moyenne = moyenne + tabscores[i];
            Moyenne = moyenne / tabscores.length;
         }
-        System.out.println(Moyenne);
+        System.out.println();
+        System.out.println("Moyenne = " + Moyenne);
 
     }
 }
